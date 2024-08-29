e25123080166fe3ef619fdf3e338d648a104ca6d
Sid <pmsidhu@gmail.com>
2024-08-29 23:19:53 +0530

    Initial Commit

---
+++ SwiftFullSourceControl/SwiftFullSourceControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftFullSourceControlApp.swift
+//  SwiftFullSourceControl
+//
+//  Created by Sid on 29/08/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftFullSourceControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
