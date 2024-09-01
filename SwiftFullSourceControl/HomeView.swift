7ec3fb38895fa59dbc3fab8219f2b7a7d205f589
Sid <pmsidhu@gmail.com>
2024-09-01 23:34:01 +0530

    Add Home View

---
+++ SwiftFullSourceControl/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  SwiftFullSourceControl
+//
+//  Created by Sid on 01/09/24.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("2nd screen")
+    }
+}
+
+#Preview {
+    HomeView()
+}
