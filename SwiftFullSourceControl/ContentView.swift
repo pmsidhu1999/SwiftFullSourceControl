9fc4c1d643803917c33a1fb9b68bf4fd7969983f
Sid <pmsidhu@gmail.com>
2024-09-01 23:30:30 +0530

    added click me button

--- SwiftFullSourceControl/ContentView.swift
+++ SwiftFullSourceControl/ContentView.swift
@@ -14,6 +14,9 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Sid")
+            Button("click me") {
+                
+            }
         }
         .padding()
     }
