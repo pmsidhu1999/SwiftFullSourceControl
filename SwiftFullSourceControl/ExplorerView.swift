7861138dcae5156480ba7dbff3f6b69f0cb2d16c
Sid <pmsidhu@gmail.com>
2024-09-06 23:46:27 +0530

    [feature] added explorer view

---
+++ SwiftFullSourceControl/ExplorerView.swift
@@ -0,0 +1,18 @@
+//
+//  ExplorerView.swift
+//  SwiftFullSourceControl
+//
+//  Created by Sid on 06/09/24.
+//
+
+import SwiftUI
+
+struct ExplorerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExplorerView()
+}
